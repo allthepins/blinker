698bdf86410e1f70e7aa4b5e978422268c02bd6e
allthepins <8884920+allthepins@users.noreply.github.com>
2025-05-10 22:50:58 +0200

    Add blinking only when button is held down

--- main/main.c
+++ main/main.c
@@ -10,26 +10,37 @@ static const char *TAG = "ATP_BLINKER";
 /**
  * GPIO for led
  */
-#define BLINK_GPIO GPIO_NUM_2 // GPIO 2 for esp32 devkit V1 built in LED
+#define BLINK_GPIO GPIO_NUM_2   // GPIO 2 for esp32 devkit V1 built in LED
+#define BUTTON_GPIO GPIO_NUM_0  // GPIO 0 for esp32 devkit V1 BOOT button
 
 void app_main(void)
 {
   /* reset pins to default state */
   gpio_reset_pin(BLINK_GPIO);
+  gpio_reset_pin(BUTTON_GPIO);
 
-  /* set LED GPIO to output only */
+  /* set GPIO directions */
   gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+  gpio_set_direction(BUTTON_GPIO, GPIO_MODE_INPUT);
+
+  /* enable pullup for input pin */
+  gpio_pullup_en(BUTTON_GPIO);
 
   while (1)
   {
-    /* LED on */
-    gpio_set_level(BLINK_GPIO, 1);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);  // wait for 1000 ms
-    ESP_LOGI(TAG, "LED on");
+    if ( gpio_get_level(BUTTON_GPIO) == 0 )
+    {
+      ESP_LOGI(TAG, "button pressed");
+      
+      /* LED on */
+      gpio_set_level(BLINK_GPIO, 1);
+      vTaskDelay(1000 / portTICK_PERIOD_MS);  // wait for 1000 ms
+      ESP_LOGI(TAG, "LED on");
 
-    /* LED off */
-    gpio_set_level(BLINK_GPIO, 0);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-    ESP_LOGI(TAG, "LED off");
+      /* LED off */
+      gpio_set_level(BLINK_GPIO, 0);
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      ESP_LOGI(TAG, "LED off");
+    }
   }
 }
